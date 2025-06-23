5febc2db4fa5403e6cdfe6ac1d139a2632dcba1a
gabrielmensah28 <gabriel.mensah@dtiafrica.edu.gh>
2025-06-23 10:45:46 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <div className='self-container'>
+        <div classNames='profile'>
+          <img src="" alt="" />
+        <h1>GABRIEL NYANTEKYI MENSAH-NKRUMAH</h1>
+        <p>I am 19 years old, a student at the Design and Technology Institute,  studying Web Development. I am passionate about technology and its potential to change the world. I enjoy learning new programming languages and frameworks, and I am always looking for opportunities to apply my skills in real-world projects.</p>
+        <p>I am a self-motivated individual with a strong work ethic and a desire to continuously improve. I am always eager to learn new things and take on new challenges. I believe
+        </p>
+        <div>
+          <h1>INTEREST</h1>
+          <div className='box-around'>Singing<h3></h3></div>
+          <div className='box-around'><h3>Writing</h3></div>
+          <div className='box-around'><h3>Programming</h3></div>
+          <div className='box-around'><h3>Football</h3></div>
+        </div>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
